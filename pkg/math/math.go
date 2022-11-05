03f97083c79a6493746dfd723c58713821f3da58
avocadohooman <gmolin@student.hive.fi>
2022-11-05 11:10:55 +0100

    added min, max, percentile and started creating csv for describe

--- pkg/math/math.go
+++ pkg/math/math.go
@@ -1,8 +1,11 @@
 package math
 
 import (
-	utils "github.com/avocadohooman/logistic-regression-magic-sorting-hat/pkg/utils"
+	"errors"
 	"math"
+	"sort"
+
+	utils "github.com/avocadohooman/logistic-regression-magic-sorting-hat/pkg/utils"
 )
 
 func Count(columns utils.Columns) int {
@@ -25,7 +28,7 @@ func Std(columns utils.Columns) float64 {
 
 	mean := Mean(columns)
 	for _, value := range columns {
-		retVal =+ (value - mean) * (value - mean)
+		retVal = +(value - mean) * (value - mean)
 	}
 	retVal = retVal / (float64)(len(columns)-1)
 	retVal = math.Sqrt(retVal)
@@ -33,12 +36,71 @@ func Std(columns utils.Columns) float64 {
 	return retVal
 }
 
-func Min() {}
+func Min(values []float64) float64 {
+	minValue := values[0]
 
-func Max() {}
+	for value, _ := range values {
+		if float64(value) < minValue {
+			minValue = float64(value)
+		}
+	}
+	return float64(minValue)
+}
+
+func Max(values []float64) float64 {
+	maxValue := values[0]
+
+	for value, _ := range values {
+		if float64(value) > maxValue {
+			maxValue = float64(value)
+		}
+	}
+	return float64(maxValue)
+}
 
 func Medium() {}
 
-func Percentile(percentile int) float64 {
-	return 0.00
+func Percentile(input []float64, percentile float64) (float64, error) {
+	if len(input) == 0 {
+		return math.NaN(), errors.New("Invalid input")
+	}
+	if len(input) == 1 {
+		return input[0], nil
+	}
+	if percentile <= 0 || percentile > 100 {
+		return math.NaN(), errors.New("Percentile range needs to be between 0 - 100")
+	}
+
+	sortedCopy := sortSlice(input)
+
+	// Multiply percent by length of input
+	// n = (P/100) x N
+	index := (percentile / 100) * float64(len(sortedCopy))
+
+	// Check if the index is a whole number
+	if index == float64(int64(index)) {
+		// Convert float to int
+		i := int(index)
+		// Find&return the value at the index
+		return sortedCopy[i-1], nil
+	} else if index > 1 {
+		// Convert float to int via truncation
+		i := int(index)
+		// Find the average of the index and following values
+		meanPercentile := Mean([]float64{sortedCopy[i-1], sortedCopy[i]})
+		return meanPercentile, nil
+	}
+	return math.NaN(), errors.New("Invalid input")
+}
+
+func sortSlice(input []float64) []float64 {
+	copiedSlice := copySlice(input)
+	sort.Float64s(copiedSlice)
+	return copiedSlice
+}
+
+func copySlice(input []float64) []float64 {
+	newSlice := make([]float64, len(input))
+	copy(newSlice, input)
+	return newSlice
 }
